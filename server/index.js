2fd3321fed14e31dd3da2bc86a7dcf6e1f363951
Scott Upman <scottupman1@gmail.com>
2022-03-12 11:16:57 -0500

    Project setup

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const pool = require("./db")
+//middleware
+app.use(cors())
+app.use(express.json())
+
+app.listen(5000, () => {
+    console.log("server has started on port 5000")
+})
