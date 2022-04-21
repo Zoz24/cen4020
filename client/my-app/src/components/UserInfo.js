10b7b12016227c62ebdb784938736f78ced706d4
Scott Upman <scottupman1@gmail.com>
2022-04-21 04:25:55 -0400

    implemented user profile page

--- client/my-app/src/components/UserInfo.js
+++ client/my-app/src/components/UserInfo.js
@@ -1,63 +1,105 @@
 import Axios from "axios";
 import React, { Fragment, useState, useEffect } from "react";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Card, Avatar, Box, Typography, Grid } from "@mui/material";
+import ProfileIcon from './batter.png'
 
-const UserInfo = () =>{
+const UserInfo = () => {
 
-const [firstname, setfirstname] = useState('')
-const [lastname, setlastname] = useState('')
-const [bday, setbday] = useState('')
-const [favteam, setfavteam] = useState('')
-const [favplayer, setfavplayer] = useState('')
-
-
-
-let uname = localStorage.getItem('USERNAME');
-
-Axios.get(`http://localhost:5000/userinfo/${uname}`,
-{
-}
-)
-.then((response) => {
-    setfirstname(response.data.fname)
-    setlastname(response.data.lname)
-    setbday(response.data.dob.slice(0,10))
-    setfavteam(response.data.favoriteteam)
-    setfavplayer(response.data.favoriteplayer)
-}) 
+    const [firstname, setfirstname] = useState('')
+    const [lastname, setlastname] = useState('')
+    const [bday, setbday] = useState('')
+    const [favteam, setfavteam] = useState('')
+    const [favplayer, setfavplayer] = useState('')
 
 
 
+    let uname = localStorage.getItem('USERNAME');
 
-return(
-  <Fragment>
-    <table align = "center">
-    <tr>
-        <th>First Name</th>
-        <td>{firstname}</td>
-    </tr>
-    <tr>
-        <th>Last Name</th>
-        <td>{lastname}</td>
-    </tr>
-    <tr>
-        <th>Username</th>
-        <td>{uname}</td>
-    </tr>
-    <tr>
-        <th>Birthday</th>
-        <td>{bday}</td>
-    </tr>
-    <tr>
-        <th>Favorite Team</th>
-        <td>{favteam}</td>
-    </tr>
-    <tr>
-        <th>Favorite Player</th>
-        <td>{favplayer}</td>
-    </tr>
-</table></Fragment>
-)
+    Axios.get(`http://localhost:5000/userinfo/${uname}`,
+        {
+        }
+    )
+        .then((response) => {
+            setfirstname(response.data.fname)
+            setlastname(response.data.lname)
+            setbday(response.data.dob.slice(0, 10))
+            setfavteam(response.data.favoriteteam)
+            setfavplayer(response.data.favoriteplayer)
+        })
+
+    return (
+        <Box sx=
+            {{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}>
+            <Card sx={{ minWidth: 500 }}>
+                <Grid
+                    container spacing={1}
+                    direction="column"
+                    alignItems="center"
+                    justifyContent="center"
+                >
+                    <Grid item xs="auto">
+                        <Avatar
+                            alt="baseball"
+                            src={ProfileIcon}
+                            sx={{ width: 200, height: 200 }}
+                        />
+                    </Grid>
+                    <Grid item xs="auto">
+                        <Typography variant = "h4" component="div">{uname}</Typography>
+                    </Grid>                   
+                </Grid>
+                {/*define a box */}
+                <Box sx = 
+                {{
+                    marginTop: 4,
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    width: 500,
+                    
+                }}
+                >
+                    <Grid
+                        container spacing={0}
+                        direction="column"
+                        alignItems="center"
+                        justifyContent="center"
+                    >
+                        <Grid item xs="auto">
+                            <Typography variant = "h6" component="div">First name:</Typography>
+                            <Typography variant = "h6" component="div">Last name:</Typography>
+                            <Typography variant = "h6" component="div">DOB:</Typography> 
+                            <Typography variant = "h6" component="div">Favorite Team: </Typography> 
+                            <Typography variant = "h6" component="div">Favorite Player:</Typography>    
+                        </Grid>
+                    </Grid>
+                    <Grid
+                        container spacing={0}
+                        direction="column"
+                        alignItems="center"
+                        justifyContent="center"
+                    >
+                        <Grid item xs="auto">
+                            <Typography variant = "h6" component="div">{firstname}</Typography>
+                            <Typography variant = "h6" component="div">{lastname}</Typography>
+                            <Typography variant = "h6" component="div">{bday}</Typography> 
+                            <Typography variant = "h6" component="div">{favteam} </Typography> 
+                            <Typography variant = "h6" component="div">{favplayer}</Typography>    
+                        </Grid>
+                    </Grid>
+                </Box>                
+            </Card>
+        </Box>
+
+    )
 }
 
 export default UserInfo;
